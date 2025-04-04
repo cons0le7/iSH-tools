65308c21d398e5bdd24a2323161bd3c99a72d2d6
ᥴ̶̧̪̤̓͊̌̽̊̎̓͜͝͠ꪮ̵͈̀͐͗͒̽͛͊͝ꪀ̸̧̨̺̮̩̣̤͂̾͜ᦓ̵̠̂̅̈́̊ͅꪮ̶̢͇̟͆͗͆́̌͗͂͒̀͜͝ꪶ̴̨̧̖͍̞̲̖̠̆̽̈́̇̾̍̒̕͠ͅꫀ̵̡̨̧̛͚͕̝̭̦̩̻̈́̚̚ͅ <162306504+cons0le7@users.noreply.github.com>
2025-04-04 22:47:13 +0000

    Add files via upload

---
+++ tools/xsstrike.sh
@@ -0,0 +1,8 @@
+cd tools
+git clone https://github.com/s0md3v/XSStrike
+echo "Installing Dependancies..."
+pip3 install tld
+pip3 install fuzzywuzzy
+pip3 install requests
+echo "Installation Complete!" 
+
