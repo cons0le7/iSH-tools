fe5aa105272d64a05d52cc1e8104340655aa68c0
ᥴ̶̧̪̤̓͊̌̽̊̎̓͜͝͠ꪮ̵͈̀͐͗͒̽͛͊͝ꪀ̸̧̨̺̮̩̣̤͂̾͜ᦓ̵̠̂̅̈́̊ͅꪮ̶̢͇̟͆͗͆́̌͗͂͒̀͜͝ꪶ̴̨̧̖͍̞̲̖̠̆̽̈́̇̾̍̒̕͠ͅꫀ̵̡̨̧̛͚͕̝̭̦̩̻̈́̚̚ͅ <162306504+cons0le7@users.noreply.github.com>
2025-04-04 14:34:26 +0000

    Add files via upload

---
+++ tools/nikto.sh
@@ -0,0 +1,9 @@
+cd tools 
+echo ""
+echo "Installing Nikto..."
+echo ""
+echo "Installing Perl..." 
+apk add perl 
+git clone https://github.com/sullo/nikto
+echo "Installation complete!"
+
---
+++ tools/nikto_ex.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+sleep 2
+cd ./tools/nikto/program || exit 1
+exec bash
