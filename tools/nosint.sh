67d9d534974ced8e9d7c791dbbf370bc74fa1603
ᥴ̶̧̪̤̓͊̌̽̊̎̓͜͝͠ꪮ̵͈̀͐͗͒̽͛͊͝ꪀ̸̧̨̺̮̩̣̤͂̾͜ᦓ̵̠̂̅̈́̊ͅꪮ̶̢͇̟͆͗͆́̌͗͂͒̀͜͝ꪶ̴̨̧̖͍̞̲̖̠̆̽̈́̇̾̍̒̕͠ͅꫀ̵̡̨̧̛͚͕̝̭̦̩̻̈́̚̚ͅ <162306504+cons0le7@users.noreply.github.com>
2025-07-08 22:10:02 +0000

    Add files via upload

---
+++ tools/nosint.sh
@@ -0,0 +1,5 @@
+echo "Installing NoSint... "
+cd $HOME/iSH-tools/tools
+git clone https://github.com/cons0le7/nosint.git
+chmod +x $HOME/iSH-tools/tools/nosint/nosint $HOME/iSH-tools/tools/nosint/update/update.sh $HOME/iSH-tools/tools/nosint/update/update_2.sh 
+echo "Installation Complete!"
